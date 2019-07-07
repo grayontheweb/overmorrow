36818eef657c3e02febc9b4f801af76e35669028
Gray <grayontheweb@gmail.com>
2019-07-07 17:38:05 +0700

    Fix

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,7 +2,7 @@ import CMS from 'netlify-cms-app';
 import uploadcare from 'netlify-cms-media-library-uploadcare';
 import cloudinary from 'netlify-cms-media-library-cloudinary';
 
-import FounderPreview from './preview-templates/Mainstay';
+import FounderPreview from './preview-templates/Founder';
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
