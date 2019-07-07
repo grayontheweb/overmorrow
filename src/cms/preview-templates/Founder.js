14b8218b66857c651854a4dcea6c567a57bfd571
Gray <grayontheweb@gmail.com>
2019-07-07 17:34:18 +0700

    Update

--- src/cms/preview-templates/Founder.js
+++ src/cms/preview-templates/Founder.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Mainstay from 'src/components/Mainstay';
+import Founder from 'src/components/Founder';
 
-const MainstayPreview = ({ entry, widgetFor }) => (
-  <Mainstay
+const FounderPreview = ({ entry, widgetFor }) => (
+  <Founder
     imageUrl={entry.getIn(['data', 'image'])}
     name={entry.getIn(['data', 'title'])}
     title={entry.getIn(['data', 'jobTitle'])}
   />
 );
 
-MainstayPreview.propTypes = {
+FounderPreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
   widgetFor: PropTypes.func,
 };
 
-export default MainstayPreview;
+export default FounderPreview;
