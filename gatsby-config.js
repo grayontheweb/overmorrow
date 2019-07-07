b1a3a0818ace82fc8d0015cd457070b76b2ed884
Gray <grayontheweb@gmail.com>
2019-07-07 15:45:36 +0700

    Test

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/static/img`,
+        path: 'static/img',
         name: 'uploads',
       },
     },
@@ -49,7 +49,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
+        modulePath: 'src/cms/cms.js',
       },
     },
     {
@@ -60,5 +60,5 @@ module.exports = {
       },
     }, // must be after other CSS plugins
     'gatsby-plugin-netlify', // make sure to keep it last in the array
-  ]
-}
+  ],
+};
