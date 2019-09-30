d2d280ece4845c3b247113307ca3472c71c4d889
Gray <grayontheweb@gmail.com>
2019-09-30 19:17:37 +0700

    Style about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,12 +2,60 @@ import React from 'react';
 
 import Section from 'src/components/Section';
 
+import pronunciationImage from './images/pronunciation.svg';
+import termImage from './images/term.svg';
+
 import './About.scss';
 
 const About = () => {
   return (
-    <Section className="About" fullScreen>
-      About
+    <Section className="About">
+      <div className="About__container">
+        <div className="About__container__column-left">
+          <div className="About__dictionary">
+            <h2
+              className="About__dictionary__term"
+              style={{ backgroundImage: `url(${termImage})` }}
+            >
+              Overmorrow
+            </h2>
+
+            <h3
+              className="About__dictionary__pronunciation"
+              style={{ backgroundImage: `url(${pronunciationImage})` }}
+            >
+              ˈō-vər-mȯr-ō
+            </h3>
+
+            <span className="About__dictionary__definition">
+              The day after or following tomorrow
+            </span>
+
+            <span className="About__dictionary__source">
+              (Webster’s English Dictionary, 1913)
+            </span>
+          </div>
+        </div>
+
+        <div className="About__container__column-right">
+          <div className="About__description">
+            <p>
+              Inspired by our namesake, an old word to talk about the future,
+              here at Overmorrow Brewing we craft beers to promote and honor
+              classic styles throughout history – while updating them for the
+              present and the future here in Vietnam.
+            </p>
+
+            <p>
+              Just like our founding team, our beers are a mix of well-traveled
+              styles from around the world with local Vietnamese flavors and
+              ingredients. We are experimental but never compromise on quality
+              and never launch anything that we are not happy to drink
+              ourselves.
+            </p>
+          </div>
+        </div>
+      </div>
     </Section>
   );
 };
