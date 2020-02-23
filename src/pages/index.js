7182d94ff57f3e856a6cec1ec45f2f2007a27213
Gray <grayontheweb@gmail.com>
2020-02-23 14:03:24 +0700

    Trying toget whereToFindUs working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,21 @@
-import React from 'react';
+// import React from 'react';
 
-import About from 'src/components/About';
-import Intro from 'src/components/Intro';
-import Layout from 'src/components/Layout';
-import Story from 'src/components/Story';
-import Founders from 'src/components/Founders';
-import Mainstays from 'src/components/Mainstays';
+// import About from 'src/components/About';
+// import Intro from 'src/components/Intro';
+// import Layout from 'src/components/Layout';
+// import Story from 'src/components/Story';
+// import Founders from 'src/components/Founders';
+// import Mainstays from 'src/components/Mainstays';
 
 export default (props) => {
-  return (
-    <Layout location={props.location}>
-      <Intro />
-      <About />
-      <Story />
-      <Founders />
-      <Mainstays />
-    </Layout>
-  );
+  return null;
+  // return (
+  //   <Layout location={props.location}>
+  //     <Intro />
+  //     <About />
+  //     <Story />
+  //     <Founders />
+  //     <Mainstays />
+  //   </Layout>
+  // );
 };
